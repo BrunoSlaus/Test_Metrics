9e08717a6d7f3d2719ad47e2a674acd81a6cc8a2
Bruno Slaus <60346417+BrunoSlaus@users.noreply.github.com>
2022-11-24 16:34:15 +0100

    Update Metricsmake.py

--- Metricsmake.py
+++ Metricsmake.py
@@ -98,69 +98,7 @@ def metrics(student_data, max_points_per_problem, limit_pass):
 
     return()
     
-'''
-#First we open the required data with astropy
-print 'Plotting both the inner and the outer part of the field.\n'                                
 
-Data=fits.open(basename + 'OUT_Single.fits')[1].data   
-Total_flux_corr = Data['Total_flux_corr']
-Spectral_index  = Data['Spectral_index']
-print  'len(Total_flux_corr):', len(Total_flux_corr)
-print  'len(Spectral_index):' , len(Spectral_index)
-
-Data_2=fits.open(basename + 'IN_Single.fits')[1].data   
-Total_flux_corr_2 = Data_2['Total_flux_corr']
-Spectral_index_2  = Data_2['Spectral_index']
-print  'len(Total_flux_corr_2):', len(Total_flux_corr_2)
-print  'len(Spectral_index_2):' , len(Spectral_index_2)
-
-
-
-#We define the limit line
-x = np.arange(0.00001, 1, 0.0001)
-alpha_limit = -(np.log10(x)-np.log10(Det_Limit)) / (np.log10(610)-np.log10(1400))
-
-
-
-#Plotting the graph
-fig = plt.figure(figsize=(7,7))
-ax = fig.add_axes([0.12, 0.10, 0.8, 0.40])
-ax.set_xlabel(r'$S_{610}/\ \mathrm{mJy}$', size=22)
-ax.set_ylabel(r'$\alpha$', size=22)
-ax.set_xlim(2*10**(-4)*1000, 10*1000)
-ax.set_ylim(-2.5, 2.5)
-plt.scatter(Total_flux_corr*1000, Spectral_index,color='black', s=2)
-plt.plot(x*1000,alpha_limit, color='black', linestyle='--')
-plt.xscale('log')
-plt.hlines(0.745486, 0.0001, 10*1000, linestyle='-', color='red')
-print 'Mean Outer Part: 0.745486'
-#plt.hlines(0.87651, 0.0001, 10, linestyle=':', color='red')
-#PLOTTING THE VERTICAL LINE OF THE LIMIT WE CUT/CROP
-plt.vlines(0.02*1000, -3, 4, linestyle='-', color='blue')
-ax.text(0.6*1000, 2, 'Vanjski dio', fontsize=16)
-
-axx = fig.add_axes([0.12, 0.50, 0.8, 0.40])
-#axx.set_xlabel(r'$S_{610}/\ mJy$', size=22)
-axx.set_ylabel(r'$\alpha$', size=22)
-axx.set_xlim(2*10**(-4)*1000, 10*1000)
-axx.set_ylim(-1.9, 2)
-plt.scatter(Total_flux_corr_2*1000, Spectral_index_2,color='black', s=2)
-plt.plot(x*1000,alpha_limit, color='black', linestyle='--')
-plt.xscale('log')
-plt.hlines(0.646233, 0.0001, 10*1000, linestyle='-', color='red')
-print 'Mean Inner Part: 0.646233'
-#plt.hlines(0.81143, 0.0001, 10, linestyle=':', color='red')
-#PLOTTING THE VERTICAL LINE OF THE LIMIT WE CUT/CROP
-plt.vlines(0.015*1000, -3, 4, linestyle='-', color='blue')
-axx.text(0.6*1000, 1.6, 'Srednji dio', fontsize=16)
-axx.set_xticklabels([], alpha=0.0)
-
-plt.savefig('Plot_Final', dpi=130)
-#plt.savefig('/home/bruno/Desktop/BIAS_SEMINAR_TOGETHER', dpi=200)
-plt.show()
-plt.close()
-
-'''
 
 
 
